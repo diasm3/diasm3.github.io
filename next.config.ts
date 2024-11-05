55bc15663696332149057236599bbcda7b72f652
SEMYUNG PARK <semyungpark@SEMYUNGs-MacBook-Pro.local>
2024-11-05 18:12:15 +0900

    init: default pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: "export", // 정적 페이지 생성 활성화
+  basePath: "/diasm3.github.io", // GitHub repository 이름으로 변경해주세요
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
